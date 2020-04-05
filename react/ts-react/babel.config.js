1d471537cb1a3d405137b1d4e65f3bf4cf2014e1
XiongAmao <onycat@live.cn>
2020-04-05 15:42:19 +0800

    chore: add babel config

--- react/ts-react/babel.config.js
+++ react/ts-react/babel.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  presets: [['@babel/preset-env', { useBuiltIns: 'entry', corejs: 3 }]],
-  plugins: [
+  presets: [
     [
-      '@babel/plugin-transform-runtime',
+      '@babel/preset-env',
       {
+        useBuiltIns: 'usage',
         corejs: 3
       }
-    ]
+    ],
+    ['@babel/preset-typescript'],
+    ['@babel/preset-react']
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-transform-runtime', { corejs: 3 }],
+    ['@babel/plugin-proposal-class-properties']
   ]
 }
